aaec1b019b33389f1078956a384e3e27154f3ed0
Gary Allen <g.allen@hud.ac.uk>
2019-11-07 10:20:12 +0000

    Updated the demo to make it clearer and make it run 'stand alone'.

--- README.md
+++ README.md
@@ -3,27 +3,37 @@ This is a demo of the JavaSpace transactions functionality.
 Written by Dr Gary Allen, University of Huddersfield.
 
 
-This demo shows how individual space operations (read, take, write, etc.) can be combined into a single 'ACID' transaction.  It takes an object out of the space, modifies the contents, and writes it back.
+This demo shows how individual space operations (read, take, write, etc.) can be combined into a single 'ACID' transaction.  The demo:
+
+Writes an SObj into the space (not as part of a transaction).
+
+Takes that object out of the space, modifies the contents, and writes it back again (all as part of a single transaction).
+
+Takes the modified object back out of the space to prove everthing worked (not as part of a transaction).
+
 
 It has been updated to Apache River 3.0.
+
 
 As usual, if using an IDE like IntelliJ or eclipse you need to create a java project with the correct classes added as libraries, then paste the code in and run it.
 You also need to create a Run Configuration and pass the correct VM arguments in order to get the code to run.  Add this:
 
-    -Djava.rmi.server.useCodebaseOnly=false -Djava.security.policy=/local/public/chs2546/policy.all
+    -Djava.rmi.server.useCodebaseOnly=false -Djava.security.policy=policy.all
 
-to the VM args of the Run Configuration.
+to the VM args of the Run Configuration.  You can then run the code directly from your IDE.
 
-NOTE - All paths mentioned here assume running the code on the university linux network.  If at home you need to amend the paths accordingly.
 
-To run the demo, you need the JavaSpaceGetAndPut demo.  Run the Putter and put a "SObj" object into the space, then run this demo (the "TxnExample" code) either from your IDE or following the instructions below, then use the Getter to retrive the object.  Its contents will have been updated.
+.
+.
 
 
 If running this code from a command line, set up the classpath with:
 
 	. jinicl
 
-then compile with:
+NOTE that the above script contains the right paths for the university linux network.  If you're running this demo at home you will need to edit it accordingly.
+
+Now compile the code with:
 
 	javac *.java
 
@@ -34,8 +44,6 @@ and run the code with:
 OR run a script which contains the above command with:
 
 	TxnRun
-
-
 
 
 
--- src/TxnExample.java
+++ src/TxnExample.java
@@ -1,59 +1,101 @@
-import java.rmi.RMISecurityManager;
-
 import net.jini.core.transaction.*;
 import net.jini.core.transaction.server.*;
 import net.jini.space.JavaSpace;
-import net.jini.core.lease.*;
 
 public class TxnExample {
+
+	private static int ONE_SECOND = 1000;  // 1000 milliseconds
+	private static int TWO_SECONDS = 2000;  // 2000 milliseconds
+	private static int THREE_SECONDS = 3000;  // 3000 milliseconds
+
 	public static void main(String[] args) {
 		// set up the security manager
 		if (System.getSecurityManager() == null)
-			System.setSecurityManager(new RMISecurityManager());
+			System.setSecurityManager(new SecurityManager());
 
+		// Find the transaction manager on the network
 		TransactionManager mgr = SpaceUtils.getManager();
-		if (mgr == null){
+		if (mgr == null) {
 			System.err.println("Failed to find the transaction manager");
 			System.exit(1);
 		}
 
+		// Find the Java Space on the network
 		JavaSpace space = SpaceUtils.getSpace();
-		if (space == null){
+		if (space == null) {
 			System.err.println("Failed to find the javaspace");
 			System.exit(1);
 		}
 
-		Transaction.Created trc = null;
+
+		// Write an object into the space so that we know that there is one to take back out again.
+		// Note that this operation is NOT part of the transaction
 		try {
-			trc = TransactionFactory.create(mgr, 3000);
+			Sobj newSobj = new Sobj("This is a test SObj");
+			space.write(newSobj, null, TWO_SECONDS);
+			System.out.println("Written the initial SObj with the message: " + newSobj.contents);
 		} catch (Exception e) {
-			System.out.println("Could not create transaction " + e);
+			System.out.println("Couldn't write the initial object to the space " + e);
+			System.exit(1);
 		}
 
-		Transaction txn = trc.transaction;
 
-		Sobj template = new Sobj();
+		// Now try to take the object back out of the space, modify it, and write it back again.
+		// All of this IS part of one single transaction, so it all happens or it all rolls back and never happens
 		try {
+			// First we need to create the transaction object
+			Transaction.Created trc = null;
 			try {
-				Sobj sobj = (Sobj)space.take(template, txn, 2000);
+				trc = TransactionFactory.create(mgr, THREE_SECONDS);
+			} catch (Exception e) {
+				System.out.println("Could not create transaction " + e);
+			}
 
+			Transaction txn = trc.transaction;
+
+			// Now take the initial object back out of the space...
+			try {
+				Sobj template = new Sobj("This is a test SObj");
+				Sobj sobj = (Sobj) space.take(template, txn, ONE_SECOND);
 				if (sobj == null) {
 					System.out.println("Error - No object found in space");
+					txn.abort();
 					System.exit(1);
+				} else {
+					System.out.println("Read the initial SObj with the message: " + sobj.contents);
 				}
 
+				// ... edit that object and write it back again...
 				sobj.contents = "A new message";
-
-				space.write(sobj, txn, Lease.FOREVER);
-			} catch ( Exception e) {
+				space.write(sobj, txn, TWO_SECONDS);
+				System.out.println("Changed the SObj to the message '" + sobj.contents + "' and written it back to the space");
+			} catch (Exception e) {
 				System.out.println("Failed to read or write to space " + e);
 				txn.abort();
 				System.exit(1);
 			}
 
+			// ... and commit the transaction.
 			txn.commit();
 		} catch (Exception e) {
 			System.out.print("Transaction failed " + e);
+		}
+
+
+		// Now read the updated message back to prove it all worked
+		// Again, this is NOT part of a transaction
+		try {
+			Sobj template = new Sobj("A new message");
+			Sobj sobj = (Sobj) space.take(template, null, ONE_SECOND);
+			if (sobj == null) {
+				System.out.println("Error - couldn't take the updated object");
+				System.exit(1);
+			} else {
+				System.out.println("Taken the updated SObj from the space with the message: " + sobj.contents);
+			}
+		}catch (Exception e) {
+			System.out.println("Failed to read or write to space " + e);
+			System.exit(1);
 		}
 
 		System.out.println("Done.  Exiting...");
